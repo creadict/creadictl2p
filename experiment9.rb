4ddcb6dddc7d8eb260b53a4bb3a79e6772f7cb42
creadict <creadict@gmail.com>
2013-06-26 16:32:28 -0700

    full name greeting

---
+++ experiment9.rb
@@ -0,0 +1,12 @@
+puts ''
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + ', :)'
+puts ''
+puts 'So you\'re first name is ' + name + '. I would ask you for you\'re middle name
+but a lot of people don\'t have one so just keep it to yourself ok?'
+puts 'What is you\'re last name?'
+last_name = gets.chomp
+puts 'Alright, good to meet you ' + name + ' ' + last_name + ' :)'
+
