61364544386c777a21a8f2cc53f56606a3984846
creadict <creadict@gmail.com>
2013-06-26 16:31:17 -0700

    bigger, better favorite number

---
+++ experiment10.rb
@@ -0,0 +1,5 @@
+puts ''
+puts 'Hello, what is you\'re favorite number?'
+number = gets.chomp
+number2 = number.to_i + 1
+puts 'Your favorite number is ' + number + ' then. Hmmm... don\'t you think the number ' + number2.to_s + ' is a bigger, better favorite number?'
